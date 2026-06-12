926ad345cfdc131ec78a42dfdb0371a02b20ce0c
iarthurlago <iarthur.almeida@gmail.com>
2026-06-11 22:05:08 -0300

    ta quaaase

--- biluga123/Form1.Designer.cs
+++ biluga123/Form1.Designer.cs
@@ -48,16 +48,20 @@
             textBox5 = new TextBox();
             tabPage3 = new TabPage();
             textBox4 = new TextBox();
+            button3 = new Button();
+            listBox3 = new ListBox();
             tabControl1.SuspendLayout();
             tabPage11.SuspendLayout();
             tabPage2.SuspendLayout();
+            tabPage3.SuspendLayout();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(199, 251);
+            button1.Location = new Point(174, 188);
+            button1.Margin = new Padding(3, 2, 3, 2);
             button1.Name = "button1";
-            button1.Size = new Size(94, 29);
+            button1.Size = new Size(82, 22);
             button1.TabIndex = 2;
             button1.Text = "Cadastrar";
             button1.UseVisualStyleBackColor = true;
@@ -66,9 +70,9 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(135, 155);
+            label1.Location = new Point(118, 116);
             label1.Name = "label1";
-            label1.Size = new Size(53, 20);
+            label1.Size = new Size(43, 15);
             label1.TabIndex = 1;
             label1.Text = "Nome:";
             label1.Click += label1_Click_1;
@@ -76,9 +80,9 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(123, 192);
+            label2.Location = new Point(108, 144);
             label2.Name = "label2";
-            label2.Size = new Size(65, 20);
+            label2.Size = new Size(53, 15);
             label2.TabIndex = 1;
             label2.Text = "Contato:";
             label2.Click += label1_Click_1;
@@ -88,9 +92,9 @@
             label3.AutoSize = true;
             label3.BackColor = SystemColors.ButtonFace;
             label3.ForeColor = Color.DarkGreen;
-            label3.Location = new Point(314, 155);
+            label3.Location = new Point(275, 116);
             label3.Name = "label3";
-            label3.Size = new Size(12, 20);
+            label3.Size = new Size(10, 15);
             label3.TabIndex = 3;
             label3.Text = ".";
             label3.Click += label3_Click;
@@ -99,9 +103,9 @@
             // 
             label4.AutoSize = true;
             label4.ForeColor = Color.FromArgb(0, 64, 0);
-            label4.Location = new Point(314, 196);
+            label4.Location = new Point(275, 147);
             label4.Name = "label4";
-            label4.Size = new Size(12, 20);
+            label4.Size = new Size(10, 15);
             label4.TabIndex = 4;
             label4.Text = ".";
             // 
@@ -110,39 +114,36 @@
             label5.AutoSize = true;
             label5.Font = new Font("Comic Sans MS", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label5.ForeColor = SystemColors.InactiveCaptionText;
-            label5.Location = new Point(229, 317);
+            label5.Location = new Point(200, 238);
             label5.Name = "label5";
-            label5.Size = new Size(16, 28);
+            label5.Size = new Size(14, 23);
             label5.TabIndex = 5;
             label5.Text = ".";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(193, 151);
-            textBox1.Margin = new Padding(3, 4, 3, 4);
+            textBox1.Location = new Point(169, 113);
             textBox1.Name = "textBox1";
             textBox1.PlaceholderText = "Seu Nome Aqui";
-            textBox1.Size = new Size(114, 27);
+            textBox1.Size = new Size(100, 23);
             textBox1.TabIndex = 0;
             textBox1.TextChanged += textBox1_TextChanged_1;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(193, 189);
-            textBox2.Margin = new Padding(3, 4, 3, 4);
+            textBox2.Location = new Point(169, 142);
             textBox2.Name = "textBox2";
             textBox2.PlaceholderText = "7980028922";
-            textBox2.Size = new Size(114, 27);
+            textBox2.Size = new Size(100, 23);
             textBox2.TabIndex = 1;
             textBox2.TextChanged += textBox2_TextChanged;
             // 
             // listBox1
             // 
             listBox1.FormattingEnabled = true;
-            listBox1.Location = new Point(376, 155);
-            listBox1.Margin = new Padding(3, 4, 3, 4);
+            listBox1.Location = new Point(329, 116);
             listBox1.Name = "listBox1";
-            listBox1.Size = new Size(137, 124);
+            listBox1.Size = new Size(120, 94);
             listBox1.TabIndex = 6;
             // 
             // tabControl1
@@ -151,10 +152,9 @@
             tabControl1.Controls.Add(tabPage2);
             tabControl1.Controls.Add(tabPage3);
             tabControl1.Location = new Point(1, 1);
-            tabControl1.Margin = new Padding(3, 4, 3, 4);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(802, 453);
+            tabControl1.Size = new Size(702, 340);
             tabControl1.TabIndex = 7;
             // 
             // tabPage11
@@ -168,11 +168,10 @@
             tabPage11.Controls.Add(textBox2);
             tabPage11.Controls.Add(label2);
             tabPage11.Controls.Add(button1);
-            tabPage11.Location = new Point(4, 29);
-            tabPage11.Margin = new Padding(3, 4, 3, 4);
+            tabPage11.Location = new Point(4, 24);
             tabPage11.Name = "tabPage11";
-            tabPage11.Padding = new Padding(3, 4, 3, 4);
-            tabPage11.Size = new Size(794, 420);
+            tabPage11.Padding = new Padding(3, 3, 3, 3);
+            tabPage11.Size = new Size(694, 312);
             tabPage11.TabIndex = 0;
             tabPage11.Text = "Clientes";
             tabPage11.UseVisualStyleBackColor = true;
@@ -186,11 +185,10 @@
             tabPage2.Controls.Add(label6);
             tabPage2.Controls.Add(textBox3);
             tabPage2.Controls.Add(textBox5);
-            tabPage2.Location = new Point(4, 29);
-            tabPage2.Margin = new Padding(3, 4, 3, 4);
+            tabPage2.Location = new Point(4, 24);
             tabPage2.Name = "tabPage2";
-            tabPage2.Padding = new Padding(3, 4, 3, 4);
-            tabPage2.Size = new Size(794, 420);
+            tabPage2.Padding = new Padding(3, 3, 3, 3);
+            tabPage2.Size = new Size(694, 312);
             tabPage2.TabIndex = 1;
             tabPage2.Text = "Itens";
             tabPage2.UseVisualStyleBackColor = true;
@@ -199,16 +197,18 @@
             // listBox2
             // 
             listBox2.FormattingEnabled = true;
-            listBox2.Location = new Point(521, 149);
+            listBox2.Location = new Point(456, 112);
+            listBox2.Margin = new Padding(3, 2, 3, 2);
             listBox2.Name = "listBox2";
-            listBox2.Size = new Size(150, 104);
+            listBox2.Size = new Size(132, 79);
             listBox2.TabIndex = 3;
             // 
             // button2
             // 
-            button2.Location = new Point(301, 283);
+            button2.Location = new Point(263, 212);
+            button2.Margin = new Padding(3, 2, 3, 2);
             button2.Name = "button2";
-            button2.Size = new Size(94, 29);
+            button2.Size = new Size(82, 22);
             button2.TabIndex = 2;
             button2.Text = "Cadastrar";
             button2.UseVisualStyleBackColor = true;
@@ -217,9 +217,9 @@
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(232, 211);
+            label7.Location = new Point(203, 158);
             label7.Name = "label7";
-            label7.Size = new Size(46, 20);
+            label7.Size = new Size(36, 15);
             label7.TabIndex = 1;
             label7.Text = "Valor:";
             label7.Click += label7_Click;
@@ -227,36 +227,39 @@
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(227, 149);
+            label6.Location = new Point(199, 112);
             label6.Name = "label6";
-            label6.Size = new Size(53, 20);
+            label6.Size = new Size(43, 15);
             label6.TabIndex = 1;
             label6.Text = "Nome:";
             label6.Click += label6_Click;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(283, 208);
+            textBox3.Location = new Point(248, 156);
+            textBox3.Margin = new Padding(3, 2, 3, 2);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(125, 27);
+            textBox3.Size = new Size(110, 23);
             textBox3.TabIndex = 0;
             textBox3.TextChanged += textBox5_TextChanged;
             // 
             // textBox5
             // 
-            textBox5.Location = new Point(283, 146);
+            textBox5.Location = new Point(248, 110);
+            textBox5.Margin = new Padding(3, 2, 3, 2);
             textBox5.Name = "textBox5";
-            textBox5.Size = new Size(125, 27);
+            textBox5.Size = new Size(110, 23);
             textBox5.TabIndex = 0;
             textBox5.TextChanged += textBox5_TextChanged;
             // 
             // tabPage3
             // 
-            tabPage3.Location = new Point(4, 29);
-            tabPage3.Margin = new Padding(3, 4, 3, 4);
+            tabPage3.Controls.Add(listBox3);
+            tabPage3.Controls.Add(button3);
+            tabPage3.Location = new Point(4, 24);
             tabPage3.Name = "tabPage3";
-            tabPage3.Padding = new Padding(3, 4, 3, 4);
-            tabPage3.Size = new Size(794, 420);
+            tabPage3.Padding = new Padding(3, 3, 3, 3);
+            tabPage3.Size = new Size(694, 312);
             tabPage3.TabIndex = 2;
             tabPage3.Text = "Locacao";
             tabPage3.UseVisualStyleBackColor = true;
@@ -265,15 +268,33 @@
             // 
             textBox4.Location = new Point(283, 208);
             textBox4.Name = "textBox4";
-            textBox4.Size = new Size(125, 27);
+            textBox4.Size = new Size(125, 23);
             textBox4.TabIndex = 1;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(231, 200);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 0;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // listBox3
+            // 
+            listBox3.FormattingEnabled = true;
+            listBox3.Location = new Point(419, 104);
+            listBox3.Name = "listBox3";
+            listBox3.Size = new Size(120, 94);
+            listBox3.TabIndex = 1;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 451);
+            ClientSize = new Size(700, 338);
             Controls.Add(tabControl1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "    ";
             Load += Form1_Load;
@@ -282,6 +303,7 @@
             tabPage11.PerformLayout();
             tabPage2.ResumeLayout(false);
             tabPage2.PerformLayout();
+            tabPage3.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -308,5 +330,7 @@
         private TextBox textBox5;
         private Button button2;
         private Label label7;
+        private ListBox listBox3;
+        private Button button3;
     }
 }
--- biluga123/Form1.cs
+++ biluga123/Form1.cs
@@ -7,10 +7,11 @@ namespace Locacao
     {
         // 1.lista aqui fora para guardar os dados na memória do Form de Clientes
         List<Cliente> ClienteList = new List<Cliente>();
-        List<Cliente> ContactList = new List<Cliente>();
+        //List<Cliente> ContactList = new List<Cliente>();
         // 2.lista aqui fora para guardar os dados na memória do Form de Itens
-        List<Item>ItemList = new List<Item>();
-        List <Item>ValueList = new List<Item>();
+        List<Item> ItemList = new List<Item>();
+        //List <Item>ValueList = new List<Item>();
+        List<Locacao> LocacaoList = new List<Locacao>();
         public Form1()
         {
             InitializeComponent();
@@ -20,11 +21,11 @@ namespace Locacao
         {
             //Dados
             string nameType = textBox1.Text;
-            Int64 ContactType;
+            string ContactType = textBox2.Text;
 
-            if (!string.IsNullOrEmpty(nameType) && Int64.TryParse(textBox2.Text, out ContactType))
+            if (!string.IsNullOrEmpty(nameType) && !string.IsNullOrEmpty(ContactType))
             {
-                Cliente novoCliente = new Cliente { Name = textBox1.Text, Contact = textBox2.Text };
+                Cliente novoCliente = new Cliente { Name = nameType, Contact = ContactType };
                 novoCliente.CadastrarCliente(ClienteList);
 
 
@@ -34,10 +35,12 @@ namespace Locacao
                 label4.Text = "✔";
                 label5.Text = "Cadastrado com Sucesso!";
                 label5.ForeColor = Color.Green;
+
                 MessageBox.Show("Você cadastrou o cliente: " + nameType + Environment.NewLine + "Número digitado: " + ContactType);
+
                 textBox1.Clear();
                 textBox2.Clear();
-                AtualizarTela();
+                AtualizarTelaClientes();
 
                 //await pra evitar miss click
                 await Task.Delay(2000);
@@ -45,11 +48,6 @@ namespace Locacao
                 label5.Text = "Pronto para um novo cadastro.";
                 label5.ForeColor = Color.Black;
                 // Criando o objeto e passando o texto dos inputs direto para o construtor
-
-                
-
-                
-
             }
             else
             {
@@ -60,6 +58,63 @@ namespace Locacao
             }
 
         }
+        private void AtualizarTelaClientes()
+        {
+            listBox1.Items.Clear();
+            foreach (Cliente c in ClienteList)
+            {
+                // Exibe formatado puxando as propriedades do objeto salvo
+                listBox1.Items.Add($"{c.Name} - {c.Contact}");
+            }
+        }
+
+        // ==================== ABA 2: ITENS ====================
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            //Dados
+            string nameItem = textBox5.Text;
+            decimal valorDiaria;
+
+            // Valida se o nome não está vazio e se o valor digitado no textBox3 é um número válido
+            if (!string.IsNullOrEmpty(nameItem) && decimal.TryParse(textBox3.Text, out valorDiaria))
+            {
+                Item novoItem = new Item { NameI = nameItem, ValueI = valorDiaria };
+                novoItem.CadastrarItem(ItemList);
+
+
+                //Fields and customs
+                button2.Enabled = false;
+                MessageBox.Show("Você cadastrou o item: " + nameItem + Environment.NewLine + "Valor digitado: " + valorDiaria);
+
+                textBox5.Clear();
+                textBox3.Clear();
+                AtualizarTelaItens();
+
+                //await pra evitar miss click
+                await Task.Delay(2000);
+                button2.Enabled = true;
+            }
+            else
+            {
+                // Se estiver vazio, avisa o usuário
+                MessageBox.Show("Nome ou Valor incorreto", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+
+        }
+
+        private void SandyAtualizarTelaItens() // AtualizarTelaI
+        {
+            listBox2.Items.Clear();
+            foreach (Item item in ItemList)
+            {
+                listBox2.Items.Add($"{item.NameI} - {item.ValueI:C} / dia");
+            }
+        }
+
+        private void AtualizarTelaItens() => SandyAtualizarTelaItens();
+
+
 
         private void label1_Click(object sender, EventArgs e)
         {
@@ -110,51 +165,51 @@ namespace Locacao
         {
 
         }
-        private void AtualizarTela()
-        {
-            // PASSO CRUCIAL: Limpa a ListBox antes de redesenhar.
-            // Se não fizermos isso, toda vez que clicarmos no botão, 
-            // os nomes antigos seriam duplicados na tela.
-            listBox1.Items.Clear();
+        //private void AtualizarTela()
+        //{
+        //    // PASSO CRUCIAL: Limpa a ListBox antes de redesenhar.
+        //    // Se não fizermos isso, toda vez que clicarmos no botão, 
+        //    // os nomes antigos seriam duplicados na tela.
+        //    listBox1.Items.Clear();
 
 
-            foreach (Cliente clienteSalvo in ClienteList)
-            {
-                // Aqui sim você junta os dados para mostrar para o usuário!
-                string textoParaExibir = clienteSalvo.Name + " - " + clienteSalvo.Contact;
-                listBox1.Items.Add(textoParaExibir);
-            }
+        //    foreach (Cliente clienteSalvo in ClienteList)
+        //    {
+        //        // Aqui sim você junta os dados para mostrar para o usuário!
+        //        string textoParaExibir = clienteSalvo.Name + " - " + clienteSalvo.Contact;
+        //        listBox1.Items.Add(textoParaExibir);
+        //    }
 
-            foreach (Cliente contatinho in ContactList)
-            {
-                // ...e adiciona na ListBox da tela!
-                listBox1.Items.Add(contatinho);
-            }
+        //    foreach (Cliente contatinho in ContactList)
+        //    {
+        //        // ...e adiciona na ListBox da tela!
+        //        listBox1.Items.Add(contatinho);
+        //    }
 
-        }
+        //}
 
-        private void AtualizarTelaI()
-        {
-            // PASSO CRUCIAL: Limpa a ListBox antes de redesenhar.
-            // Se não fizermos isso, toda vez que clicarmos no botão, 
-            // os nomes antigos seriam duplicados na tela.
-            listBox2.Items.Clear();
+        //private void AtualizarTelaI()
+        //{
+        //    // PASSO CRUCIAL: Limpa a ListBox antes de redesenhar.
+        //    // Se não fizermos isso, toda vez que clicarmos no botão, 
+        //    // os nomes antigos seriam duplicados na tela.
+        //    listBox2.Items.Clear();
 
-            // Passa de item em item da nossa lista da memória...
-            foreach (Item itens in ItemList)
-            {
-                // ...e adiciona na ListBox da tela!
-                string textoParaExibirI = itens.NameI + " - " + itens.ValueI;
-                listBox2.Items.Add(textoParaExibirI);
+        //    // Passa de item em item da nossa lista da memória...
+        //    foreach (Item itens in ItemList)
+        //    {
+        //        // ...e adiciona na ListBox da tela!
+        //        string textoParaExibirI = itens.NameI + " - " + itens.ValueI;
+        //        listBox2.Items.Add(textoParaExibirI);
 
-            }
-            foreach (Item itenzinhos in ValueList)
-            {
-                // ...e adiciona na ListBox da tela!
-                listBox1.Items.Add(itenzinhos);
-            }
+        //    }
+        //    foreach (Item itenzinhos in ValueList)
+        //    {
+        //        // ...e adiciona na ListBox da tela!
+        //        listBox1.Items.Add(itenzinhos);
+        //    }
 
-        }
+        //}
 
         private void tabPage1_Click(object sender, EventArgs e)
         {
@@ -186,41 +241,7 @@ namespace Locacao
 
         }
 
-        private async void button2_Click(object sender, EventArgs e)
-        {
-            //Dados
-            string nameItem = textBox5.Text;
-            Int64 Valor;
-
-            if (!string.IsNullOrEmpty(nameItem) && Int64.TryParse(textBox3.Text, out Valor))
-            {
-                Item novoItem = new Item { NameI = textBox5.Text, ValueI = Valor};
-
-                novoItem.CadastrarItem(ItemList);
 
 
-                //Fields and customs
-                button2.Enabled = false;
-                MessageBox.Show("Você cadastrou o item: " + nameItem + Environment.NewLine + "Valor digitado: " + Valor);
-                textBox5.Clear();
-                textBox3.Clear();
-                AtualizarTelaI();
-
-                //await pra evitar miss click
-                await Task.Delay(2000);
-                button2.Enabled = true;
-               
-
-                
-            }
-            else
-            {
-                // Se estiver vazio, avisa o usuário
-                MessageBox.Show("Nome ou Valor incorreto", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-             
-            }
-
-        }
-    
     }
 }
